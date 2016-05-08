0fdf47ee1e7ba53592692c788e97274b7f404ca5
Timm Knape <timm@kna-st.de>
2016-05-08 11:01:43 +0200

    one pass interpreter

--- tie/Node.m
+++ tie/Node.m
@@ -1,15 +1,13 @@
 #import "Node.h"
 
-@interface Node ()
-
-    - (NSAttributedString *) evaluateFrom: (const char *) begin end: (const char *) end;
-
-@end
-
-NSInteger expression(const char **begin, const char *end);
-
-NSInteger factor(const char **begin, const char *end) {
+static void eat_whitespace(const char **begin, const char *end) {
     for (; *begin != end && **begin <= ' '; *begin += 1);
+}
+
+static NSInteger expression(const char **begin, const char *end);
+
+static NSInteger factor(const char **begin, const char *end) {
+    eat_whitespace(begin, end);
     if (*begin == end) { return 0; }
     if (isdigit(**begin) || **begin == '-') {
         int factor = 1;
@@ -23,16 +21,17 @@ NSInteger factor(const char **begin, const char *end) {
     } else if (**begin == ')') {
         *begin += 1;
         NSInteger result = expression(begin, end);
+        eat_whitespace(begin, end);
         if (*begin != end && **begin == ')') { *begin += 1; }
         return result;
     }
     return 0;
 }
 
-NSInteger term(const char **begin, const char *end) {
+static NSInteger term(const char **begin, const char *end) {
     NSInteger prod = factor(begin, end);
     while (*begin != end) {
-        for (; *begin != end && **begin <= ' '; *begin += 1);
+        eat_whitespace(begin, end);
         if (*begin == end) { break; }
         char op = **begin;
         if (op != '*' && op != '/' && op != '%') { break; }
@@ -47,10 +46,10 @@ NSInteger term(const char **begin, const char *end) {
     return prod;
 }
 
-NSInteger expression(const char **begin, const char *end) {
+static NSInteger expression(const char **begin, const char *end) {
     NSInteger sum = term(begin, end);
     while (*begin != end) {
-        for (; *begin != end && **begin <= ' '; *begin += 1);
+        eat_whitespace(begin, end);
         if (*begin == end) { break; }
         char op = **begin;
         if (op != '+' && op != '-') { break; }
@@ -64,59 +63,59 @@ NSInteger expression(const char **begin, const char *end) {
     return sum;
 }
 
+static NSAttributedString *evaluateText(const char **begin, const char *end);
+
+static NSAttributedString *interpretBlock(const char **begin, const char *end) {
+    eat_whitespace(begin, end);
+    if (*begin == end) { return NSAttributedString.new; }
+    if (isdigit(**begin) || **begin == '-' || **begin == '(') {
+        NSInteger result = expression(begin, end);
+        NSNumber *number = [NSNumber numberWithInteger: result];
+        NSNumberFormatter *formatter = NSNumberFormatter.new;
+        NSAttributedString *attr = [[NSAttributedString alloc] initWithString:[formatter stringFromNumber: number]];
+        eat_whitespace(begin, end);
+        if (*begin != end && **begin == '}') { *begin += 1; }
+        return attr;
+    } else if (memcmp("bold ", *begin, 5) == 0) {
+        *begin += 5;
+        NSAttributedString *sub = evaluateText(begin, end);
+        NSMutableAttributedString *result = [[NSMutableAttributedString alloc] initWithAttributedString: sub];
+        [result addAttribute: NSFontAttributeName value: [NSFont boldSystemFontOfSize: 12] range: NSMakeRange(0, sub.length)];
+        return result;
+    } else return NSAttributedString.new;
+}
+
+static NSAttributedString *evaluateText(const char **begin, const char *end) {
+    if (*begin == end) { return NSAttributedString.new; }
+    
+    const char *cur;
+    NSMutableAttributedString *result = NSMutableAttributedString.new;
+    while (*begin != end && **begin != '}') {
+        for (cur = *begin; cur != end  && *cur != '{' && *cur != '}'; ++cur) {}
+        if (cur != *begin) {
+            NSString *str = [[NSString alloc] initWithBytes:*begin length: cur - *begin encoding:NSUTF8StringEncoding];
+            [result appendAttributedString: [[NSAttributedString alloc] initWithString: str]];
+            *begin = cur;
+        }
+        
+        if (*begin != end && **begin == '{') {
+            *begin += 1;
+            [result appendAttributedString: interpretBlock(begin, end)];
+        }
+    }
+    return result;
+}
+
 @implementation Node
 
     - (void)setSource:(NSString *)source {
         _source = source;
     }
 
-    - (NSAttributedString *) interpretFrom: (const char *) begin end: (const char *) end {
-        if (isdigit(*begin) || *begin == '-' || *begin == '(') {
-            NSInteger result = expression(&begin, end);
-            NSNumber *number = [NSNumber numberWithInteger: result];
-            NSNumberFormatter *formatter = NSNumberFormatter.new;
-            NSAttributedString *attr = [[NSAttributedString alloc] initWithString:[formatter stringFromNumber: number]];
-            return attr;
-        } else if (memcmp("bold ", begin, 5) == 0) {
-            NSAttributedString *sub = [self evaluateFrom: begin + 5 end: end];
-            NSMutableAttributedString *result = [[NSMutableAttributedString alloc] initWithAttributedString: sub];
-            [result addAttribute: NSFontAttributeName value: [NSFont boldSystemFontOfSize: 12] range: NSMakeRange(0, sub.length)];
-            return result;
-        } else return NSAttributedString.new;
-    }
-
-    - (NSAttributedString *) evaluateFrom: (const char *) begin end: (const char *) end {
-    
-        if (begin == end) { return NSAttributedString.new; }
-        
-        const char *cur;
-        NSMutableAttributedString *result = NSMutableAttributedString.new;
-        while (begin != end) {
-            for (cur = begin; cur != end  && *cur != '{'; ++cur) {}
-            if (cur != begin) {
-                NSString *str = [[NSString alloc] initWithBytes:begin length: cur - begin encoding:NSUTF8StringEncoding];
-                [result appendAttributedString: [[NSAttributedString alloc] initWithString: str]];
-                begin = cur;
-            }
-            
-            if (begin != end && *begin == '{') {
-                const char *start = ++begin;
-                for (int count = 1; begin != end && count; ++begin) {
-                    switch (*begin) {
-                        case '{': ++count; break;
-                        case '}': --count; break;
-                    }
-                }
-                [result appendAttributedString: [self interpretFrom: start end:begin - 1]];
-            }
-        }
-        return result;
-    }
-
     - (NSAttributedString *) evaluated {
         const char *begin = self.source.UTF8String;
         const char *end = begin + strlen(begin);
-        return [self evaluateFrom: begin end: end];
+        return evaluateText(&begin, end);
     }
 
 @end
